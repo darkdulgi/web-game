98ba4050d4c7fcb6268667d2d7e8a75e8d93c1ec
darkdulgi <daniel99285@gmail.com>
2024-10-03 14:06:59 +0900

    [fix] 마우스 클릭 유지중을 나타내는 전역상태에서 좌클릭 제외한 입력 배제시킴

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { useEffect } from "react";
 import useMouseStore from "./common/store/mouseStore";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./router";
+import { MOUSE_LEFT } from "./common/constants";
 
 export default function App() {
   const setMouseState = useMouseStore((state) => state.setMouseState);
 
   useEffect(() => {
-    function handleMouseDown() {
+    function handleMouseDown(e: MouseEvent) {
+      if (e.button !== MOUSE_LEFT) return;
       setMouseState(true);
     }
-    function handleMouseUp() {
+    function handleMouseUp(e: MouseEvent) {
+      if (e.button !== MOUSE_LEFT) return;
       setMouseState(false);
     }
+
     window.addEventListener("mousedown", handleMouseDown);
     window.addEventListener("mouseup", handleMouseUp);
     return () => {
