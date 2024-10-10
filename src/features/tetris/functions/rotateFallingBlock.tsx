8217e597287f114510fb260505299a774eeff512
darkdulgi <daniel99285@gmail.com>
2024-10-10 13:32:29 +0900

    [feat] L블록 회전 동작 구현

--- src/features/tetris/functions/rotateFallingBlock.tsx
+++ src/features/tetris/functions/rotateFallingBlock.tsx
@@ -9,6 +9,8 @@ export default function rotateFallingBlock(
   if (!field.length || !r) return;
 
   const fallingBlockList: number[][] = [];
+  const cur = [...fallingBlock.current];
+  let centerPos: number[] = [];
 
   field.forEach((row, xpos) => {
     row.forEach((value, ypos) => {
@@ -17,6 +19,21 @@ export default function rotateFallingBlock(
       }
     });
   });
-  if (fallingBlock.current[0] === TETROMINO.OSCAR) return;
+  if (cur[0] === TETROMINO.OSCAR) return;
+  if (cur[0] === TETROMINO.INDIA) {
+    const dx = [0.5, 0.5, -0.5, 0.5];
+    const dy = [0.5, -0.5, 0.5, 0.5];
+    centerPos = [fallingBlockList[1][0] + dx[cur[1]], fallingBlockList[1][1] + dy[cur[1]]];
+  }
 
+  if (centerPos.length) {
+    fallingBlockList.forEach((block) => {
+      field[block[0]][block[1]] = TETRIS_BOX.EMPTY;
+    });
+    fallingBlockList.forEach((block) => {
+      field[centerPos[0] + block[1] - centerPos[1]][centerPos[1] + centerPos[0] - block[0]] =
+        TETRIS_BOX.FALLING;
+    });
+    fallingBlock.current[1] = (fallingBlock.current[1] + 1) % 4;
+  }
 }
