c5179df713f5a97473a6a5c85e22ea692d822706
darkdulgi <daniel99285@gmail.com>
2024-10-23 15:41:30 +0900

    [feat] 테트리스 이미지 프리로딩 적용

--- src/pages/tetris/index.tsx
+++ src/pages/tetris/index.tsx
@@ -7,6 +7,20 @@ export default function TetrisPage() {
     <>
       <Helmet>
         <title>테트리스</title>
+        <link rel="preload" href="/tetris/blue_block.png" as="image" />
+        <link rel="preload" href="/tetris/blue_T.png" as="image" />
+        <link rel="preload" href="/tetris/cyan_block.png" as="image" />
+        <link rel="preload" href="/tetris/cyan_Z.png" as="image" />
+        <link rel="preload" href="/tetris/green_block.png" as="image" />
+        <link rel="preload" href="/tetris/green_L.png" as="image" />
+        <link rel="preload" href="/tetris/pink_block.png" as="image" />
+        <link rel="preload" href="/tetris/pink_J.png" as="image" />
+        <link rel="preload" href="/tetris/purple_block.png" as="image" />
+        <link rel="preload" href="/tetris/purple_I.png" as="image" />
+        <link rel="preload" href="/tetris/red_block.png" as="image" />
+        <link rel="preload" href="/tetris/red_O.png" as="image" />
+        <link rel="preload" href="/tetris/yellow_block.png" as="image" />
+        <link rel="preload" href="/tetris/yellow_S.png" as="image" />
       </Helmet>
       <Container>
         <Tetris />
