67da3d1b01abe53da2604f497c34f951aa861033
darkdulgi <daniel99285@gmail.com>
2024-11-18 12:48:25 +0900

    [feat] 모바일 환경에서 햄버거 메뉴 간단 구현:

--- src/common/components/topbar.tsx
+++ src/common/components/topbar.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Topbar() {
+  const [menuVisible, setMenuVisible] = useState<boolean>(false);
   return (
-    <nav className="flex justify-between px-2 py-1 xl:hidden w-full bg-neutral-800 text-white">
+    <nav className="flex relative justify-between px-2 py-1 xl:hidden w-full bg-neutral-800 text-white">
       <div className="flex items-center gap-3">
-        <img className="h-10 w-10" src="/burger.svg" />
+        <button onClick={() => setMenuVisible((x) => !x)} className="h-10 w-10">
+          <img src="/burger.svg" alt="햄버거" />
+        </button>
 
-        <Link to="/" className="text-xl font-bold">Web Game</Link>
+        <Link to="/" className="text-xl font-bold">
+          Web Game
+        </Link>
+      </div>
+
+      <div
+        className={`absolute transition flex flex-col left-0 top-full z-30 px-5 py-2 gap-2 bg-neutral-700 border-4 border-neutral-800 ${!menuVisible && "-translate-x-full"}`}
+      >
+        <Link to="/minesweeper">지뢰찾기</Link>
+        <Link to="/tetris">테트리스</Link>
+        <Link to="/snake">뱀 게임</Link>
       </div>
     </nav>
   );
